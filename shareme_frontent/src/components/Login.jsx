3e836d94a44f72e193e100d72a985a619e1c3b2e
JacekHacking <Jacekhacking@mac.com>
2023-01-03 14:05:37 -0700

    develop branch created new files added

---
+++ shareme_frontent/src/components/Login.jsx
@@ -0,0 +1,20 @@
+import GoogleLogin from "react-google-login";
+import { useNavigate } from "react-router";
+import { FcGoogle } from "react-icons";
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
+const Login = () => {
+  return (
+    <>
+      <div className="flex justify-start items-center flex-col h-screen">
+        Login
+      </div>
+      <div className="relative w-full h-full">
+        <video src=""></video>{" "}
+      </div>
+    </>
+  );
+};
+
+export default Login;
